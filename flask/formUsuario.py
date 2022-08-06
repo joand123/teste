26d3e46893f6664b1a10e206592bc38660394ba7
joand123 <85586274+joand123@users.noreply.github.com>
2022-08-06 17:46:03 -0300

    Update formUsuario.py

--- flask/formUsuario.py
+++ flask/formUsuario.py
@@ -1,10 +1,12 @@
 from flask-wtf import Flaskform
 from wtforms import StrinField,SubmitField,PassworField
-from wtforms,Validators import DataRequired
+from wtforms.field.html5 import EmailField
+from wtforms.Validators import DataRequired
 
 class  UsuariosForms(FlaskForms):
-    nome = StringField('Nome da Chave' , validators=[DataRequired()])
-    username =  StringField('Nome do Usuario')
-    senha = StringField('Senha: ')
+    nome = StringField('Nome da Chave: ' , validators=[DataRequired()])
+    username =  StringField('Nome do Usuario: ', validator=[dataRequred()])
+    telefone = StringField('telefone: ', validator=[dataRequred()])
+    senha = StringField('Senha: ', validator=[dataRequred()])
     enviar = SubmitField('CADASTRAR')
     
