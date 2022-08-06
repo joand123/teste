fe0fc1807486ce2e069bd0607a78deac10808806
joand123 <85586274+joand123@users.noreply.github.com>
2022-08-06 16:48:45 -0300

    Update Usuario.py

--- flask/Usuario.py
+++ flask/Usuario.py
@@ -1,13 +1,9 @@
-import cProfile
-from enum import unique
-from ssl import _PasswordType
-from unicodedata import name
 from database  import db
 
 class Usuario(db.Model):
     id = db.column(db.Integer, primary_Key=True)
-    name = db.Column(db.String(180), unique=False, nulable=False)
+    nome  = db.Column(db.String(180), unique=False, nulable=False)
     username = db.Column(db.String(120), unique=True, nulable=False)
-    cpf = db.Column(db.String(100), unique=True, nulable=True) 
-    password = db.Column(db.String(100), unique=False, nulable=False) 
     email = db.Column(db.String(180), unique=True, nulable=True) 
+    telefone = db.Column(db.String(40), unique=False, nulable=True)
+    senha = db.column(db,String(100), unique=False, nulable=False)
